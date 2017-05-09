968431390d28c804d9387525bf470ffe3ea07eb9
Woody Christy <woody@woodychristy.com>
2017-05-08 19:03:07 -0700

    Add files via upload

--- scripts/prepareDrives.sh
+++ scripts/prepareDrives.sh
@@ -130,7 +130,8 @@ getHostnames(){
 getFirstNode(){
     firstNode=$VM_NAME_PREFIX$(printf %0${VMSS_NUM_LENGTH}d 0)
     log $firstNode " First Node's hostname'. Hostname of this node: "$(hostname -s) 
-    if [[ $(hostname -s) = $firstNode ]]; then
+    host = $( hostname -s )
+    if [[ $host = $firstNode ]]; then
        log "------- First node! Generating hostnames and running install -------"
        sudo mkdir -p $INVENTORY_FILE_DIR
        getHostnames $VM_NAME_PREFIX $NUM_VMS
@@ -150,7 +151,7 @@ log "------- Determining if first node -------"
 log $VM_NAME_PREFIX
 log $NUM_VMS
 
-getFirstNode $VM_NAME_PREFIX
+getFirstNode 
 
 
 
