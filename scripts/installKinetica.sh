7eab6e79d1ebe3c555eb1091332e4033c56cfebe
Woody Christy <woody@woodychristy.com>
2017-05-18 20:35:40 -0700

    Add files via upload

--- scripts/installKinetica.sh
+++ scripts/installKinetica.sh
@@ -146,10 +146,7 @@ VM_NAME_PREFIX=$2
 VMSS_NUM_LENGTH=6
 declare -i NUM_VMS=$3
 
-#Log input params for debugging
-log $SSHPASS
-log $VM_NAME_PREFIX
-log $NUM_VMS
+
 
 #Remove existing keys
 [ -e ~/.ssh/id_rsa ] && rm ~/.ssh/id_rsa*
@@ -219,10 +216,41 @@ for i in $DST_IPs; do
     $SSHPASS_CMD  rsync -ar "$SSH_KEY_DIR/." "$i:$SSH_KEY_DIR/."
 done
 
-for i in $DST_IPs; do
-    $SSHPASS_CMD  rsync -ar "$SSH_KEY_DIR/." "$i:$SSH_KEY_DIR/."
-done
 
+#Now we need to do this for GPUDB user
+
+
+
+GPUDB_USER_HOME=$(eval echo ~gpudb)
+GPUDB_KEY_DIR=$GPUDB_USER_HOME/.ssh
+GPUDB_TMP_SSH_FOLDER="/tmp/gpudbssh"
+GPUDB_TMP_AUTH_KEYFILE=$GPUDB_TMP_SSH_FOLDER/authorized_keys
+GPUB_TMP_KEYFILE=$GPUDB_TMP_SSH_FOLDER/id_rsa.pub
+
+#Remove existing keys
+[ -e $GPUDB_USER_HOME/.ssh/id_rsa ] && rm $GPUDB_KEY_DIR/id_rsa*
+
+mkdir -p $GPUDB_TMP_SSH_FOLDER
+
+ssh-keygen -t rsa -N "" -f $GPUDB_TMP_SSH_FOLDER/id_rsa
+#Make a copy of good known hosts file 
+cp $KNOWN_HOSTS_FILE $GPUDB_TMP_SSH_FOLDER/.
+cat $GPUB_TMP_KEYFILE > $GPUDB_TMP_AUTH_KEYFILE
+
+for i in ${DST_IPs[@]}; do
+
+  ssh $i  mkdir -p $GPUDB_TMP_SSH_FOLDER
+  #copy
+
+  rsync -avr "$GPUDB_TMP_SSH_FOLDER/." "$i:$GPUDB_TMP_SSH_FOLDER/."
+
+  #permissions
+  ssh $i sudo chmod -R 644 $GPUDB_TMP_SSH_FOLDER/
+  ssh $i sudo chmod 600 $GPUDB_TMP_SSH_FOLDER/id_rsa
+  ssh $i sudo cp -r $GPUDB_TMP_SSH_FOLDER/. $GPUDB_KEY_DIR/.
+  ssh $i sudo chown -R gpudb:gpudb $GPUDB_KEY_DIR/.
+  ssh $i sudo rm -rf $GPUDB_TMP_SSH_FOLDER
+done
 
 log "All done."
 
